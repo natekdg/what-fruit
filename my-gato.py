03501246395d33d48bc107bd42a5ec6bab40882b
nathan <nathankdgray@gmail.com>
2023-12-04 21:11:25 -0800

    first commit
    - added folders for my cats
    - added added video function to enable camera

--- my-gato.py
+++ my-gato.py
@@ -1,2 +1,14 @@
 import cv2
 
+
+
+
+
+cap = cv2.VideoCapture(0)       # default view for camera
+
+while True:
+    ret, frame = cap.read()         # gather frame from one camera
+    if not ret:
+        break
+
+    cv2.imshow('logicam', frame)        # show the frame
