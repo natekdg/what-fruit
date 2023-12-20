c6c62eab06b18eeeb941f9b0b1ac890d27edce75
nathan <nathankdgray@gmail.com>
2023-12-20 13:21:30 -0800

    refactoring
    - was running into issues with tensorflow
    - going to go ahead and switch over and try a new method for training yolo with images

--- YOLOmodel.py
+++ YOLOmodel.py
@@ -1,42 +1,11 @@
-import cv2
-import numpy as np
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
+import tensorflow as tf
 
-model = MobileNetV2(weights='imagenet')
+def load_model():
+    model = tf.saved_model.load("yolov4")
+    return model
 
-def preprocess(frame):
-    # resize to model input size
-    frame_resized = cv2.resize(frame, (224, 224))
-
-    # conver t to numpy array and process
-    frame_array = np.array(frame_resized, dtype=np.float32)
-    frame_preprocessed = preprocess_input(frame_array)
-
-    return frame_preprocessed
-
-cap = cv2.VideoCapture(0)       # use the default camera
-
-while True:
-    ret, frame = cap.read()  # Read a frame from the camera
-    if not ret:
-        break
-
-    processed_frame = preprocess(frame)
-
-    # predict using preprocessed frame
-    predictions = model.predict(np.expand_dims(processed_frame, axis=0))
-    top_prediction = decode_predictions(predictions, top=1)[0][0]
-
-    # display top of the frame
-    cv2.putText(frame, top_prediction[1], (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-    # show frame with prediction
-    cv2.imshow('Webcam', frame)
-
-    # close webcam if q is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# release and close all cv2 windows
-cap.release()
-cv2.destroyAllWindows()
+def get_predictions(model, image):
+    image_array = tf.convert_to_tensor(image)
+    image_array = tf.expand_dims(image_array, 0)
+    predictions = model(image_array)
+    return predictions
--- webcam-tracking.py
+++ webcam-tracking.py
@@ -1,10 +1,3 @@
 import cv2
-import torch
-from model import load_model
-
-# apply and load the model
-model = load_model
-
-# allow access to the webcam
-cap = cv2.VideoCapture(0)
+from YOLOmodel import load_model, get_predictions       # load predictions from YOLOmodel.py file
 
